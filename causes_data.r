ba17385c503173c600b490bea9f85a539f59151b
Devanshi Desai <devanshidesai@Devanshis-MacBook-Air.local>
2023-05-28 12:43:10 -0700

    fixed error in publishing

--- causes_data.r
+++ causes_data.r
@@ -3,5 +3,5 @@ library("plotly")
 library("ggplot2")
 library("tidyr")
 
-causes_data <- read.csv("~/Desktop/info201/final-project-devanshidesai25/NOAA_Reef_Check__Bleaching_Data.csv")
+causes_data <- read.csv("NOAA_Reef_Check__Bleaching_Data.csv")
 
