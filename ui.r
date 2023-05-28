9286a6ec5d7ca9042bd4b941c974cc1298f7cf11
Yingpeng Cheng <yingpc@uw.edu>
2023-05-28 15:14:26 -0700

    change

--- ui.r
+++ ui.r
@@ -8,10 +8,66 @@ source("causes_data.r")
 library("bslib")
 
 my_ui <- navbarPage(
-  theme = bs_theme(bootswatch = "minty"),
   title = "A Global Analysis of Coral Reef Bleachings",
   id = "navbar",
-  tabPanel("Introduction"),
+  tabPanel("Introduction",
+           fluidPage(
+             titlePanel("Welcome to A Global Analysis of Coral Reef Bleachings"),
+             sidebarLayout(
+               sidebarPanel(
+                 h3("Overview"),
+                 p("Welcome to the \"A Global Analysis of Coral Reef Bleachings\" project, an interactive exploration of coral reef health and the factors contributing to bleaching events worldwide. This project aims to answer key questions about the extent, causes, and geographical distribution of coral reef bleachings. By analyzing and visualizing relevant data, we strive to deepen our understanding of these critical marine ecosystems and raise awareness for their conservation."),
+                 h3("Key Questions"),
+                 tags$ol(
+                   tags$li("What is the global extent of coral reef bleachings?"),
+                   tags$li("How do different oceans vary in terms of coral reef bleachings?"),
+                   tags$li("What are the main factors causing coral reef bleachings?"),
+                   tags$li("How has the severity of coral reef bleachings changed over time?")
+                 ),
+                 h3("Data Sources"),
+                 p("To answer these questions, we utilize multiple datasets:"),
+                 tags$ol(
+                   tags$li(
+                     tags$strong("Coral Reef Bleaching Data:"), " Source: [INSERT DATA SOURCE URL]",
+                     p("This dataset provides information on the occurrence and severity of coral reef bleachings across different regions and time periods. It includes data on bleaching levels, geographical coordinates of reef locations, and additional attributes.")
+                   ),
+                   tags$li(
+                     tags$strong("Oceanic Data:"), " Source: [INSERT DATA SOURCE URL]",
+                     p("This dataset contains information on various oceans, including their boundaries, characteristics, and ecological factors that may influence coral reef health.")
+                   ),
+                   tags$li(
+                     tags$strong("Causes of Coral Reef Bleaching:"), " Source: [INSERT DATA SOURCE URL]",
+                     p("This dataset offers insights into the primary causes of coral reef bleachings, such as human impact, dynamite fishing, pollution, and other factors. It includes data on the frequency and severity of each cause over time.")
+                   )
+                 ),
+                 h3("Ethical Considerations and Limitations"),
+                 p("When working with such data, it is important to acknowledge and address ethical considerations and limitations:"),
+                 tags$ol(
+                   tags$li(
+                     tags$strong("Data Privacy and Consent:"), 
+                     " It is crucial to ensure that the data used in this project adheres to appropriate privacy guidelines and has been obtained with proper consent from relevant stakeholders."
+                   ),
+                   tags$li(
+                     tags$strong("Representativeness and Bias:"), 
+                     " The available data may not capture the entire spectrum of coral reef bleachings worldwide, potentially leading to biases and limitations in our analysis. It is important to interpret the results while considering the representativeness of the dataset."
+                   ),
+                   tags$li(
+                     tags$strong("Data Accuracy and Quality:"), 
+                     " Although efforts have been made to ensure data accuracy, there may still be errors or discrepancies present. Additionally, variations in data quality and collection methods across different sources may impact the reliability of the analysis."
+                   )
+                 )
+               ),
+               mainPanel(
+                 h3("Image Placeholder"),
+                 img(src = "[INSERT IMAGE URL]", width = "100%")
+               )
+             )
+           )
+  ),
+  
+  
+  
+  
   tabPanel("Map of Coral Reef Bleachings",
            h3("Coral Reef Bleachings around the World"),
            sidebarLayout(
